a116611f2e32ec00023f65a447075682069e09d1
Jorge Prieto <jorgefprietol@gmail.com>
2021-10-13 17:35:40 -0500

    fetch - AWAIT - ASYNC

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import './App.css';
 import PageWrapper from './PageWrapper';
 import Paginacion from './Paginacion';
 import Pelicula from './Pelicula';
-import peliculasJson from './peliculas.json';
 import {useState} from 'react';
 
 function App() {
 	const [paginaActual, setPaginaActual] = useState(1);
+	const [peliculasData, setPeliculasData] = useState([]);
 	const TOTAL_POR_PAGINA = 2;
-	let peliculas = peliculasJson;
-	peliculas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA, paginaActual * TOTAL_POR_PAGINA);
+	//let peliculas = peliculasJson;
+
+	const buscarPeliculas = async () => {
+		let url = 'https://61675b72ba841a001727c2e6.mockapi.io/api/peliculas/peliculas';
+		var resultado = await fetch(url, {
+			"method": 'GET',
+			"headers": {
+				"Accept": 'application/json',
+				"Content-Type": 'application/json'
+			}
+		})
+		let json = await resultado.json();
+		setPeliculasData(json);
+	}
+	buscarPeliculas();
+	
 	const getTotalPaginas = () => {
-		let canttotalpeliculas = peliculasJson.length;
+		let canttotalpeliculas = peliculasData.length;
 		return Math.ceil(canttotalpeliculas / TOTAL_POR_PAGINA); 
 	}
+
   return (
 	  <PageWrapper>
-		{peliculas.map(pelicula => {
+		{peliculasData.map(pelicula => {
 				return (
 					<Pelicula
 						titulo={pelicula.titulo}
